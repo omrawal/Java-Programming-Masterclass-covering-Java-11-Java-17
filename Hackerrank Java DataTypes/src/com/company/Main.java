0befe890d17412a7ed32abd0195ecfb10e627c6f
Om Rawal <omrawal007@gmail.com>
2021-09-15 21:27:50 +0530

    solved codechef challenge

--- Hackerrank Java DataTypes/src/com/company/Main.java
+++ Hackerrank Java DataTypes/src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int ans=-1;
+            int n= sc.nextInt();
+            int s= sc.nextInt();
+            int arrSum=(n*(n+1))/2;
+            for (int j=1;j<=n;j++){
+                if(arrSum-j==s){
+                    ans=j;
+                    break;
+                }
+            }
+            System.out.println(ans);
+        }
+        sc.close();
 
     }
 }
